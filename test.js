c8f30e74d0cd74617c06c2313f17a4cbd55a7f01
Allan Fernandes <Fernzallan14@gmail.com>
2023-03-23 13:49:52 +0000

    assets

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-  var preloader = document.querySelector('.elementor-13944');
-  var preloaderProgressBar = document.querySelector('.elementor-progress-bar');
-//   var loadingText = preloader.querySelector('.elementor-heading-title');
+  var preloader = document.querySelector('.elementor-element-your-preloader-selector');
+  var preloaderProgressBar = preloader.querySelector('.elementor-progress-wrapper');
+  var loadingText = preloader.querySelector('.elementor-heading-title');
   var loadingPercentage = 0;
   var loadingSpeed = 50; // Change this value to manipulate the speed; lower is faster
 
-//   function updateLoadingText() {
-//     loadingText.innerHTML = 'Freshers Experience Loading ' + loadingPercentage + '%';
-//   }
+  function updateLoadingText() {
+    loadingText.innerHTML = 'Freshers Experience Loading ' + loadingPercentage + '%';
+  }
 
   function increasePercentage() {
     if (loadingPercentage < 100) {
       loadingPercentage += 1;
+      updateLoadingText();
       preloaderProgressBar.style.width = loadingPercentage + '%';
       setTimeout(increasePercentage, loadingSpeed);
     } else {
       preloader.style.display = 'none';
     }
   }
+
   increasePercentage();
 });
-
